788e2efe165696dd4f4f2dad47acfa88f3cce939
Erik Örtenberg <erikort@student.chalmers.se>
2021-12-02 01:25:45 +0100

    day1 done

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    input <- readFile "../input.txt"
+    let s = map read (words input) :: [Integer]
+    --mapM_ putStrLn $ map show (filter (\x -> x>0) [y-x | x <- s, y <- (removeFirst s)])
+    putStrLn (show $ length (filter (\x -> x>0) (zipWith difference s (removeFirst s))))
+    
+
+difference :: Integer -> Integer -> Integer
+difference x y = y - x
+
+removeFirst :: [Integer] -> [Integer]
+removeFirst [] = []
+removeFirst (x:xs) = xs
