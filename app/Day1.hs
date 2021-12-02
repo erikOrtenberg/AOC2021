bcf28078a2ddfbd938a3f317d4b77121701e5a80
Erik Örtenberg <erikort@student.chalmers.se>
2021-12-02 13:24:50 +0100

    bruh1

--- app/Day1.hs
+++ app/Day1.hs
@@ -5,5 +5,5 @@ day1 = do
     input <- readFile "../input/day1.txt"
     let s = map read (words input) :: [Integer]
     putStrLn "Day 1:"
-    putStrLn $ show $ length $ filter (\x -> x>0) (zipWith (\x y -> y - x) s (drop 1 s))
-    putStrLn $ show $ length $ filter (\x -> x>0) (zipWith (\x y -> y - x) s (drop 3 s))
+    putStrLn $ show $ length $ filter (>0) (zipWith (\x y -> y - x) s (drop 1 s))
+    putStrLn $ show $ length $ filter (>0) (zipWith (\x y -> y - x) s (drop 3 s))
--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,17 @@
 module Main where
 
+import System.Exit
+
 import Day1
 import Day2
 
 main :: IO ()
 main = do 
-    day1
-    day2
-    
+    putStrLn "Välj en dag lmao:"
+    x <- getLine
+    case x of 
+        "1" -> day1
+        "2" -> day2
+        "q" -> putStrLn "okej hejdå :)" >> exitSuccess 
+        _   -> putStrLn "Idiotjävel, skriv rätt"
+    main
