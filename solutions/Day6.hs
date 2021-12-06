37bc02476201ed628d101c52ed970ba61ed4cf0e
Erik Örtenberg <erikort@student.chalmers.se>
2021-12-06 11:10:58 +0100

    finished day6

--- solutions/Day6.hs
+++ solutions/Day6.hs
@@ -1,4 +1,21 @@
 module Day6 where
 
+import GetInput (getFile)
+import Data.List
+import Data.List.Split (chunksOf, splitOn)
+
 day6 :: IO()
-day6 = putStrLn ""
+day6 = do
+    ss <- getFile "day6.txt"
+    let fish = convertToAmount $ map read $ splitOn "," ss ::[Int]
+    print $ sum $ (iterate updateFishies fish)!!80
+    print $ sum $ (iterate updateFishies fish)!!256
+    
+convertToAmount :: [Int] -> [Int] 
+convertToAmount list = map (\x -> length $ filter (==x) list) [0..8]  
+
+updateFishies :: [Int] -> [Int]
+updateFishies [] = []
+updateFishies (x:xs) 
+    | x > 0 = zipWith (+) (xs ++ (repeat 0)) ((take 6 (repeat 0)) ++ [x,0,x]) 
+    | otherwise = (xs ++ (repeat 0))  
