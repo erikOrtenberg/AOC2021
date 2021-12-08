15a450793617ec3416cbef9128a08a3d257134f0
Erik Örtenberg <erik.ortenberg@gmail.com>
2021-12-08 14:46:44 +0100

    finished day8

--- solutions/Day7.hs
+++ solutions/Day7.hs
@@ -11,8 +11,8 @@ day7 = do
     ss <- getFile "day7.txt"
     let crabs = foldr (\x -> M.insertWith (+) x 1) M.empty $ map read $ splitOn "," ss :: Map Int Int
     let ((minCrab, a), (maxCrab, b)) = (M.findMin crabs, M.findMax crabs)
-    print $ minimum $ map (\x -> getFuelCost (\k v -> (abs (x - k)) * v) crabs) [minCrab .. maxCrab] 
-    print $ minimum $ map (\x -> getFuelCost (\k v -> (abs (x - k)) * (1 + abs (x - k)) * v  `div` 2) crabs) [minCrab .. maxCrab] 
+    print $ minimum $ map (\x -> getFuelCost (\k v -> abs (x - k) * v) crabs) [minCrab .. maxCrab] 
+    print $ minimum $ map (\x -> getFuelCost (\k v -> abs (x - k) * (1 + abs (x - k)) * v  `div` 2) crabs) [minCrab .. maxCrab] 
 
 getFuelCost ::  (Int -> Int -> Int) -> Map Int Int -> Int
 getFuelCost f m = sum . M.elems $ M.mapWithKey f m
--- solutions/Day8.hs
+++ solutions/Day8.hs
@@ -1,4 +1,49 @@
 module Day8 where
 
+import GetInput (getFile)
+import Data.List
+import Data.List.Split (chunksOf, splitOn)
+import Data.Map (Map)
+import qualified Data.Map as M
+
 day8 :: IO()
-day8 = putStrLn ""
+day8 = do
+    ss <- getFile "day8.txt"
+    let measurment = map ((\(x:y:_) ->  [words x, words y] ) . splitOn "|") (lines ss)
+    --print $ concatMap (!!1) measurment
+    print $ sum $ map (getAllOfSameLength (concatMap (!!1) measurment)) ([2,3,4,7] :: [Int])
+    print $ sum $ zipWith findCode (map (!!0) measurment) (map (!!1) measurment)
+
+findCode :: [String] -> [String] -> Int
+findCode cypher output = convertOutput (orderCypher (map sort cypher)) (map sort output)
+
+orderCypher :: [String] -> [String]
+orderCypher unsortedCypher = [zero,one,two,three,four,five,six,seven,eight,nine]
+    where
+        (one:_) = filter (\x -> 2 == length x) unsortedCypher
+        (four:_) = filter (\x -> 4 == length x) unsortedCypher
+        (seven:_) = filter (\x -> 3 == length x) unsortedCypher
+        (eight:_) = filter (\x -> 7 == length x) unsortedCypher
+        (three:_) = filter (\x -> length x == 5 && length (intersect one x) == 2) unsortedCypher
+        (nine:_) = (filter (\x -> length (intersect three x) == 5) unsortedCypher) \\ [eight, three]
+        zeroSix = filter (\x -> length (eight \\ x) == 1) unsortedCypher \\ [nine]
+        (zero:_) = filter (\x -> length (intersect one x) == 2) zeroSix
+        (six:_) = zeroSix \\ [zero]
+        twoFive = unsortedCypher \\ [zero,one,three,four,six,seven,eight,nine]
+        (two:_) = filter (\x -> length (intersect nine x) == 4) twoFive
+        (five:_) = twoFive \\ [two]
+
+convertOutput :: [String] -> [String] -> Int
+convertOutput _ [] = 0
+convertOutput lookup (x:output)
+    | index < 0 = 0
+    | otherwise = 10^(length output) * index + convertOutput lookup output
+    where
+        index = extractMaybe $ elemIndex x lookup
+
+extractMaybe :: Maybe Int -> Int
+extractMaybe (Just x) = x
+extractMaybe Nothing = -1
+
+getAllOfSameLength :: [String] -> Int -> Int
+getAllOfSameLength list len  = length $ filter (\x -> length x == len) list
