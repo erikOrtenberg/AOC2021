8e5c00d2239d3451a27dbd1eced9b886a9f3b587
Erik Örtenberg <erik.ortenberg@gmail.com>
2021-12-15 12:58:43 +0100

    finsihed day9

--- solutions/Day9.hs
+++ solutions/Day9.hs
@@ -4,41 +4,44 @@ import GetInput (getFile)
 import Data.List
 import Data.List.Split (chunksOf, splitOn)
 import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as M
+
 
 day9 :: IO()
 day9 = do
     ss <- getFile "day9.txt"
-    let depthMap = map (map read . tail . splitOn "") (lines ss) :: [[Int]]
-    let nothing = replicate (length (head depthMap)) 10
-    --print depthMap
-    print $ sum $ zipWith3 getDangerLevel (nothing : depthMap) (map convertLine depthMap) (tail depthMap ++ [nothing])
-
-n = replicate (length "7678934989765431234965432345679567899987999876543210198965019878921987654343498765678932445678998678") 0 ::[Int]
-mm = convertLine $ map read $ tail $ splitOn "" "7678934989765431234965432345679567899987999876543210198965019878921987654343498765678932445678998678"
-nn = map read $ tail $ splitOn "" "4567929878965420129874301286789456789976898989654321987894329767990986543232349874589321234567987567" ::[Int]
-z = [1,2,3,4,5,6,6,2] :: [Int]
-t = convertLine [5,1,2,3,5,1,2,0] 
-lol= [9,3,5,6,2,4,5,2] :: [Int]
-
-t0= replicate 3 Nothing :: [Maybe Int]
-t1= [Just 1, Nothing, Nothing] :: [Maybe Int]
+    let depthMap = convertDepthMapToMap $ map (map read . tail . splitOn "") (lines ss) :: Map (Int, Int) Int
+    print $ M.foldl (\x y -> x + y + 1) 0 $ getAllLowPoints depthMap
+    print $ product $ take 3 $ sortBy (flip compare) $ M.elems $ M.mapWithKey (\k _ -> findBasinSize depthMap (99,99) k) (getAllLowPoints depthMap)
 
 
-nope = replicate (length (head testDay9)) 0 ::[Int]
-testDay9 =  map (map read . tail . splitOn "") (lines "2199943210\n3987894921\n9856789892\n8767896789\n9899965678") :: [[Int]]
 
-getDangerLevel :: [Int] -> [Maybe Int] -> [Int] -> Int
-getDangerLevel _ [] _ = 0
-getDangerLevel (top:xs) (y:ys) (bottom:zs)
-    | mid < top && mid < bottom = 1 + mid + getDangerLevel xs ys zs
-    | otherwise = getDangerLevel xs ys zs
+convertDepthMapToMap :: [[Int]] -> Map (Int, Int) Int
+convertDepthMapToMap xs = foldl (\m (x,y) -> M.insert (x,y) (xs!!y!!x) m ) M.empty [(x,y) | x <- [0 .. length (head xs)-1], y <- [0.. length xs-1]]
+
+getAllLowPoints :: Map (Int,Int) Int -> Map (Int,Int) Int
+getAllLowPoints m = M.filterWithKey (\x _ -> isLowPoint m x) m
+
+isLowPoint :: Map (Int, Int) Int -> (Int, Int) -> Bool
+isLowPoint m (x,y) = lUp (x+1,y) > val && lUp (x-1,y) > val && lUp (x,y+1) > val && lUp (x,y-1) > val
     where
-        mid = fromMaybe 10 y
+        val = fromMaybe 0 $ M.lookup (x,y) m
+        lUp p = fromMaybe 10 $ M.lookup p m
 
-convertLine :: [Int] -> [Maybe Int]
-convertLine xs = zipWith3 convertElem (10:xs) xs (tail xs ++ [10])
+findBasinSize :: Map (Int, Int) Int -> (Int, Int) -> (Int, Int) -> Int
+findBasinSize m size p = length $ nub $ findBasinCoords m size p
 
-convertElem :: Int -> Int -> Int -> Maybe Int
-convertElem left mid right
-    | mid < left && mid < right = Just mid
-    | otherwise = Nothing
+findBasinCoords :: Map (Int, Int) Int -> (Int,Int) ->(Int, Int) -> [(Int, Int)]
+findBasinCoords m size (x,y)
+    | x < 0 || y < 0 || x > fst size || y > snd size = []
+    | otherwise = (x,y) : concatMap (findBasinCoords m size . fst) (filter tempF coords)
+    where
+        coords = [((x,y+1),top > mid && top /= 9),((x,y-1),bottom > mid && bottom /= 9),((x-1,y), left > mid && left /= 9),((x+1,y),right > mid && right /= 9)]
+        mid = fromMaybe 0 $ M.lookup (x,y) m
+        top = fromMaybe 10 $ M.lookup (x,y+1) m
+        bottom = fromMaybe 10 $ M.lookup (x,y-1) m
+        left = fromMaybe 10 $ M.lookup (x-1,y) m
+        right = fromMaybe 10 $ M.lookup (x+1,y) m
+        tempF :: ((Int, Int), Bool) -> Bool
+        tempF val = snd val
